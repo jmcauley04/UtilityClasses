80ebc1d281cf992a822f067ef2041be5a1c4dec3
Michael Cauley <jmcauley04@gmail.com>
2022-04-30 15:03:44 -0400

    Cleaned up the console messages

---
+++ UtilityClasses/BatchHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace UtilityClasses;
+
+/// <summary>
+/// Sometimes the amount of data that needs to be acted on is too large and needs to be batched.  
+/// This helper function handles that scenario.
+/// </summary>
+public class BatchHandler
+{
+    public Action<int>? UpdatePercentComplete { get; set; }
+
+    public async Task BatchAndExecute<T>(IEnumerable<T> items, int batchSize, Func<IEnumerable<T>, Task> func)
+    {
+        var itemList = items.ToList();
+        var chunkQty = (int)Math.Ceiling(itemList.Count * 1.0 / batchSize);
+
+        var iteration = 0;
+        foreach (var chunk in Chunk(items.ToList(), batchSize))
+        {
+            await func.Invoke(chunk);
+
+            UpdatePercentComplete?.Invoke(++iteration * 100 / chunkQty);
+        }
+    }
+
+    private static IEnumerable<IEnumerable<T>> Chunk<T>(IList<T> items, int size)
+    {
+        int iteration = 0;
+
+        while (iteration * size < items.Count)
+        {
+            yield return items
+                .Skip(size * iteration++)
+                .Take(size);
+        }
+    }
+
+}
--- UtilityClasses/Program.cs
+++ UtilityClasses/Program.cs
@@ -1,8 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-using UtilityClasses;
+﻿using UtilityClasses;
 
 Console.WriteLine("Hello, World!");
 
-await BatchAPICalls.RunExample();
+await RunExample();
 
 Console.WriteLine("Finished");
+
+static async Task RunExample()
+{
+    // initialize a BatchHandler class
+    var batchApiCalls = new BatchHandler();
+
+    // This is an example method that would be used to update the UI
+    void updateConsole(int percent) => Console.WriteLine($"\nWe are {percent}% completed");
+
+    // This is an example method that acts on each chunk of data
+    async Task myTask(IEnumerable<int> items)
+    {
+        Console.Write($"\n\nStarting {items.Count()} items with range {items.First()} to {items.Last()}... ");
+        await Task.Delay(1500);
+        Console.WriteLine("Complete!");
+    }
+
+    // This is subscribing the progress reports to the method that updates the UI
+    batchApiCalls.UpdatePercentComplete += updateConsole;
+
+    // an example collection of data and batch size
+    var items = Enumerable.Range(0, 1000);
+    var batchSize = 72;
+
+    try
+    {
+        // This is the workhorse of the operation
+        // The method will chunk the data into, at most, the batch size and perform the task on each
+        // batch of data
+        await batchApiCalls.BatchAndExecute(items, batchSize, myTask);
+    }
+    finally
+    {
+        // This is unsubscribing the progress reports to the method that updates the UI
+        batchApiCalls.UpdatePercentComplete -= updateConsole;
+    }
+}
